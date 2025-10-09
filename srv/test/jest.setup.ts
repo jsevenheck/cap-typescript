87843ae0a47bbcc5fac70bf79ea737db89f89e59
jsevenheck <163674456+jsevenheck@users.noreply.github.com>
2025-10-09 11:20:59 +0200

    Implement employee validations and outbox cleanup

--- srv/test/jest.setup.ts
+++ srv/test/jest.setup.ts
@@ -1,5 +1,6 @@
 import 'ts-node/register';
 import cds from '@sap/cds';
 
-cds.env.features.typescript = true;
+const env = cds.env as any;
+env.features.typescript = true;
 process.env.NODE_ENV = 'test';
